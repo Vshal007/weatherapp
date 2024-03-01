742cb416075fe41cdede01d20ba5ac704a4dd5fc
Vshal007 <vshalpatel018@gmail.com>
2024-03-02 00:09:43 +0530

    Weather app deployed

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,3 @@
 <h1>Welcome to SvelteKit</h1>
-<h1 class="text-3xl font-bold underline">Hello world!</h1>
+<h1 class="text-3xl font-bold underline">Weather App under progress!</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
