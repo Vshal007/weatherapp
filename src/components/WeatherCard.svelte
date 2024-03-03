8ca18644950b61b9067868cae99ed59cc2265026
Vshal007 <vshalpatel018@gmail.com>
2024-03-03 14:37:07 +0530

    responsiveness improved

--- src/components/WeatherCard.svelte
+++ src/components/WeatherCard.svelte
@@ -16,21 +16,21 @@
 </script>
 
 <div
-	class=" flex items-center justify-center gap-12 p-4 duration-500 border border-gray-300 rounded-md lg:w-[50%] md:w-[60%] w-[95%] hover:shadow-md hover:shadow-purple-500 text-amber-400 hover:cursor-pointer"
+	class=" flex sm:flex-row flex-col items-center justify-center gap-12 p-4 duration-500 border border-gray-300 rounded-md lg:w-[50%] md:w-[60%] w-[95%] hover:shadow-md hover:shadow-purple-500 text-amber-400 hover:cursor-pointer"
 >
-	<div class="px-5 w-[60%]">
+	<div class="p-5 sm:w-[40%] w-full">
+		<img
+			src="https://raw.githubusercontent.com/visualcrossing/WeatherIcons/6231688b36311be3ed337868e322258c1cb5f2f3/SVG/2nd%20Set%20-%20Color/{weatherData.icon}.svg"
+			alt="icon"
+			class="h-28"
+		/>
+	</div>
+	<div class="px-5 sm:w-[60%] w-full">
 		<h2 class="text-xl font-bold text-pink-600">{weatherData.locationName}</h2>
 		<p>Temperature: {weatherData.temp}°F</p>
 		<p>Description: {weatherData.description}</p>
 		<p>Wind Speed: {weatherData.windSpeed} mph</p>
 		<p>Humidity: {weatherData.humidity}%</p>
 		<p>UV Index: {weatherData.uvIndex}</p>
-	</div>
-	<div class="px-5 w-[40%]">
-		<img
-			src="https://raw.githubusercontent.com/visualcrossing/WeatherIcons/6231688b36311be3ed337868e322258c1cb5f2f3/SVG/2nd%20Set%20-%20Color/{weatherData.icon}.svg"
-			alt="icon"
-			class="h-28"
-		/>
 	</div>
 </div>
--- src/components/WeatherForecast.svelte
+++ src/components/WeatherForecast.svelte
@@ -5,22 +5,22 @@
 <div class="grid lg:grid-cols-2 md:grid-cols-1 grid-cols-1 gap-4 my-5">
 	{#each forecastData as day}
 		<div
-			class="p-4 flex items-center gap-12 border border-gray-300 rounded-md duration-500 hover:shadow-md hover:shadow-fuchsia-400 text-cyan-400 hover:cursor-pointer"
+			class="p-4 flex sm:flex-row flex-col items-center gap-12 border border-gray-300 rounded-md duration-500 hover:shadow-md hover:shadow-fuchsia-400 text-cyan-400 hover:cursor-pointer"
 		>
-			<div class="px-5 w-[60%]">
+			<div class="p-5 sm:w-[40%] w-full">
+				<img
+					src="https://raw.githubusercontent.com/visualcrossing/WeatherIcons/6231688b36311be3ed337868e322258c1cb5f2f3/SVG/2nd%20Set%20-%20Color/{day.icon}.svg"
+					alt="icon"
+					class="h-28"
+				/>
+			</div>
+			<div class="p-5 sm:w-[60%] w-full">
 				<h3 class="font-semibold text-pink-500">{day.date}</h3>
 				<p>Temperature: {day.temp}°F</p>
 				<p>Description: {day.description}</p>
 				<p>Wind Speed: {day.windSpeed} mph</p>
 				<p>Humidity: {day.humidity}%</p>
 				<p>UV Index: {day.uvIndex}</p>
-			</div>
-			<div class="px-5 w-[40%]">
-				<img
-					src="https://raw.githubusercontent.com/visualcrossing/WeatherIcons/6231688b36311be3ed337868e322258c1cb5f2f3/SVG/2nd%20Set%20-%20Color/{day.icon}.svg"
-					alt="icon"
-					class="h-28"
-				/>
 			</div>
 		</div>
 	{/each}
