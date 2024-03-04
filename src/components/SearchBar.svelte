b7c92647ae9db8d1bac5a5ace3a4986d16a26727
Vshal007 <vshalpatel018@gmail.com>
2024-03-04 14:24:52 +0530

    searchbar responsiveness improved 2

--- src/components/SearchBar.svelte
+++ src/components/SearchBar.svelte
@@ -16,11 +16,11 @@
 		type="text"
 		placeholder="Enter city name"
 		bind:value={cityName}
-		class="sm:px-4 sm:py-2 px-3 py-1 rounded-md sm:mr-2 bg-slate-600 hover:shadow-md hover:shadow-purple-500 duration-500"
+		class="sm:px-4 sm:py-2 px-3 py-2 rounded-md sm:mr-2 bg-slate-600 hover:shadow-md hover:shadow-purple-500 duration-500"
 	/>
 	<button
 		on:click={search}
-		class="sm:px-4 sm:py-2 px-2 py-1 bg-blue-500 text-white rounded-md hover:bg-blue-800 hover:shadow-md hover:shadow-purple-500 duration-500"
+		class="sm:px-4 sm:py-2 px-2 py-2 bg-blue-500 text-white rounded-md hover:bg-blue-800 hover:shadow-md hover:shadow-purple-500 duration-500"
 		>Search</button
 	>
 </div>
